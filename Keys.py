4291b298346abc8dcebd781a9c8fd5030dc8c236
Pointy Orb <152461798+Pointy-Orb@users.noreply.github.com>
2023-11-29 23:09:33 -0500

    Changed default subtitle directory

--- Keys.py
+++ Keys.py
@@ -1,15 +1,11 @@
 class Key: 
     def __init__(self, title: str, ep_names: list, ex_names: list, ep_nos: list, ex_nos: list):  
         #Title ID is the specification of where a video is in a DVD playlist. It can be seen by going into VLC>Playback>Title>The highlighted title ID is the title ID of the current video
-        #Write the title of the movie/show here:
         self.title = title
 
-        #Names of each episode go here. If your DVD is a movie, or doesnt have episodes, change to "none"
         self.episode_names = ep_names
-        #Respectively for each of the episodes specified above, input the title ID of that video. If(Title ID explained above)
         self.episode_title_no = ep_nos
 
-        #Names of each of the extras videos go here. Works the same as episode_names
         self.extras_names = ex_names
         #Functions the same as episode_title_no, but now with the extras instead of the episodes.
         self.extras_title_no = ex_nos
@@ -40,7 +36,8 @@ class Key:
         else: print("\nThis DVD does not have extras")
 
 def GetSubtitleDirectory():
-    return "C:\\Users\\point\\Desktop\\DVD Key\\Subtitles\\"
+    	#This is the directory that mpv-open-dvd checks for the subtitles. I
+	return "C:\\"
 
 key_names = {
 	#DONTREMOVEME
