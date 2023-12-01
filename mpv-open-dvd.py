82775426770c937f1e160e0c01273d2d50e33943
Pointy Orb <152461798+Pointy-Orb@users.noreply.github.com>
2023-11-30 21:08:48 -0500

    Updated to accomodate for new scripts

--- mpv-open-dvd.py
+++ mpv-open-dvd.py
@@ -1,28 +1,15 @@
-
-'''
-original designer:
- Pointy Orb
- 11/29/2023
-
-Built off of Get_IMDb_Movie.py
-   Ethan Miller
-   3/13/2019
-'''
-
 language_codes = {
-  ".en": "English", ".jp": "Japanese"
+  ".en": "English", ".jp": "Japanese", ".es": "Spanish", ".it": "Italian"
 }
 
 key_location = "no_directory"
 question = "null"
 
 try:
- from pyperclip import copy
  from win32api import GetVolumeInformation, GetLogicalDrives
  from win32file import GetDriveType
  import os 
  import subprocess
- import sys
  import shutil
 
 except ModuleNotFoundError as e:
@@ -37,113 +24,11 @@ except ModuleNotFoundError as e:
 
 try:
   import Keys
-except:
-  print("Keys file not found. Check to make sure that it is in the same directory as this script.")
+  import GetDVDInfo
+except ModuleNotFoundError as e:
+  print(e + " file not found. Check to make sure that it is in the same directory as this script.")
   print("Location of file: " + os.path.curdir)
   exit()
-
-def getVolumeLabel(drive):
- '''
- Reads in a disk drive ("D:\\") and returns its volume label.
- '''
- # Return name of movie from DVD Drive.
- volume_information = GetVolumeInformation(drive)
- volume_label = volume_information[0]
- return volume_label
-
-
-def getDrives():
- '''
- Searches for local disk drives matching letters A-Z
- that are in use and returns a list of the results.
- '''
- # Create an empty list to hold the drives.
- drives = []
- bitmask = GetLogicalDrives()
- # Test for each possible drive letter.
- for letter in list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
-   # If drive exists, append to list of drives.
-   if bitmask & 1:
-     drives.append(letter)
-   bitmask >>= 1
-
-
- return drives
-
-
-def getDriveTypes():
- '''
- Searches a list of disk drives and returns a
- dictionary of the same list as keys with
- drive type information as values.
- '''
- # Create an empty dictionary to hold the drive / drive types.
- types = {}
- # Create a list of drive types for typeIndex to match.
- driveTypes = ['DRIVE_UNKNOWN', 'DRIVE_NO_ROOT_DIR', 'DRIVE_REMOVABLE', 'DRIVE_FIXED', 'DRIVE_REMOTE', 'DRIVE_CDROM', 'DRIVE_RAMDISK']
-
-
- for drive in getDrives():
-   try:
-     # GetDriveType() will return a number that represents a drive type.
-     typeIndex = GetDriveType(u"%s:\\"%drive)
-     # Add new key / value pairs to the 'types' dictionary, where the drive letter is the key and the type is the value.
-     types[f'{drive}:'] = driveTypes[typeIndex]
-   except Exception as e:
-     print('Error: ', e)
- return types
-
-
-def getCdRoms():
- '''
- Searches a dictionary of drives and
- drive types and returns a list of
- drives that are CD ROMs.
- '''
- drives = getDriveTypes()
- cdroms = []
- for drive, drive_type in drives.items():
-   if drive_type == 'DRIVE_CDROM':
-     cdroms.append(drive)
- return cdroms
-
-
-def selectCdRom():
- '''
- Tests how many CD ROMs are in the machine,
- and if more than 1, prompts the user to
- select which CD ROM to use.
- '''
- cdroms = getCdRoms()
-
-
- if len(cdroms) > 1:
-   # Print the available drives.
-   for letter in cdroms:
-     print(f'{cdroms.index(letter)+1}. {letter}')
-   # Input validation.
-   while True:
-     try:
-       # Promt user to select drive.
-       index = input('Please select which drive to search (by number): ')
-       return cdroms[int(index)-1]
-     except IndexError:
-       print('Index error: That number doesn\'t appear to match any of the disk drives listed.')
-       continue
-     except ValueError:
-       print('Value error: Value must be a numeric digit. Try again and God bless.')
-       continue
-     except Exception as e:
-       print('Opps! Something went wrong. ', e)
-       continue
- else:
-   return cdroms[0]
-
-def makeVariableSafe(string: str):
-  for letter in string:
-    if letter == " ":
-      string = string.replace(letter, "_")
-  return string
 
 def getKeyValues():
   answer_title = input("What is the title of the movie/TV show on the DVD? : ")
@@ -156,10 +41,12 @@ def getKeyValues():
   answer_ex_no = []
   answer_current_ex_no = "null"
   print("Now type the titles of all the episodes on the DVD, in sequential order, then type 'done'. If your DVD does not have episodes, just type 'done' instead of inputting anything")
-  while not answer_current_ep_name == "done":
+  while True:
       answer_current_ep_name = input("Title of episode " + str(len(answer_episode_names) + 1) + ": ")
       if not answer_current_ep_name == "done":
         answer_episode_names.append(answer_current_ep_name)
+      else:
+        break
   print("\nType the Title ID for each of the episodes. You can find an episodes's Title ID by manually testing 'mpv dvd://[title ID - 1]/[dvd volume] --dvd-device=PATH'")
   for i in answer_episode_names:
     while not str.isnumeric(answer_current_ep_no):
@@ -167,8 +54,8 @@ def getKeyValues():
         answer_current_ep_no = input("Title ID of movie: ")
       else:
         answer_current_ep_no = input("Title ID of episode " + str(answer_episode_names.index(i) + 1) + " of " + str(len(answer_episode_names)) + ": ")
-        if not str.isnumeric(answer_current_ep_no):
-          print("The value you have entered is not a number. Try again:\n")
+      if not str.isnumeric(answer_current_ep_no):
+        print("The value you have entered is not a number. Try again:\n")
     answer_ep_no.append(answer_current_ep_no)
     answer_current_ep_no = "null"
   print("\nNow repeat the same thing for the extras:")
@@ -225,7 +112,7 @@ def writeToKeys(an_title: str, an_ep_names: list, an_ex_names: list, an_ep_no: l
     print("Written to local database.")
 
 # Get the volume label of your CD ROM.
-drive = selectCdRom()
+drive = GetDVDInfo.selectCdRom()
 
 key_location = os.path.abspath("C:\\Program Files (x86)\\mpv")
 
@@ -234,7 +121,7 @@ def CloneList(copy_from: list, copy_to: list):
   for i in copy_from:
      copy_to.append(i)
 
-label_volume = getVolumeLabel(drive)
+label_volume = GetDVDInfo.getVolumeLabel(drive)
 
 
 title = "null"
